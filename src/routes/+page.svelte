a3073fefa3eb6ada9fca90fb5d31240d5e85e3b1
Michel Marinho <michel.marinho04@outlook.com>
2022-10-14 14:16:55 -0400

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>Alura Cook</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<a href="/receitas">Ir para as receitas</a>
